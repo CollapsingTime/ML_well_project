c716382482dc2bbd366e57bd369db0c8a5e57321
CollapsingTime <vladislavrumiantsev@gmail.com>
2024-04-26 12:13:13 +0300

    Create Class and function 'read_path'

--- read_results.py
+++ read_results.py
@@ -2,35 +2,50 @@ import shutil
 import time
 import os
 
-
-BASE_DIR = 'E:\ml_models\ALL_CASES\RESULTS\\base_case_100\\result.log'
-
-DIR_NAME = 'E:\ml_models\ALL_CASES\RESULTS'
+# Put in your RESULT directory
+BASE_RESULT_DIR = '/home/vladislav/data/ML_well_project/_DATA/RESULTS'
 
 TOTAL_RESULTS = dict()
 
-count = 0
-for root, dirs, files in os.walk(DIR_NAME):
-    while count < 2:
-        with open(f"{DIR_NAME}\\{dirs[count]}\\result.log", 'r', encoding='utf-8') as file:
-            temp = []
-            for line in file:
-                if 'TGP=' in line:
-                    start = line.find(' TGP=')
-                    end = line.find(', TGPH')
-                    temp_res = line[start+5:end]
-                    temp.append(float(temp_res))
-            temp_res = []
-            for num in range(len(temp)-1):
-                temp_res.append(temp[num+1]-temp[num])
+class Read_results:
+    def __init__(self, path: str, data: dict = {}):
+        self.path = path
+        self.data = data
 
-            TOTAL_RESULTS.setdefault(dirs[count], {}).setdefault('GAS', temp_res)
-        count += 1
+    def read_path(self, all_dirs: set = set()) -> set:
+        """
+        Function pick up and collect all files name in directory
+        """
+        for root, dirs, files in os.walk(self.path):
+            all_dirs.update(dirs)
+        return all_dirs
 
-print(TOTAL_RESULTS)
-print(sum(TOTAL_RESULTS['base_case_100']['GAS']))
+test = Read_results(BASE_RESULT_DIR)
+test.read_path()
 
-res = []
+# count = 0
+# for root, dirs, files in os.walk(BASE_RESULT_DIR):
+#     # while count < 7:
+#         # You should change / or \ depending on OS
+#         with open(f"{BASE_RESULT_DIR}/{dirs[count]}/result.log", 'r', encoding='utf-8') as file:
+#             temp = []
+#             for line in file:
+#                 if 'TGP=' in line:
+#                     start = line.find(' TGP=')
+#                     end = line.find(', TGPH')
+#                     temp_res = line[start+5:end]
+#                     temp.append(float(temp_res))
+#             temp_res = []
+#             for num in range(len(temp)-1):
+#                 temp_res.append(temp[num+1]-temp[num])
+
+#             TOTAL_RESULTS.setdefault(dirs[count], {}).setdefault('GAS', temp_res)
+#         count += 1
+
+# print(TOTAL_RESULTS)
+# print(sum(TOTAL_RESULTS['CASE_254__PERM_10__Len_500__H_5__C5_300']['GAS']))
+
+# res = []
 
 # with open(BASE_DIR, 'r', encoding='utf-8') as file:
 #     for line in file:
@@ -42,20 +57,20 @@ res = []
 
 # print(res)
 
-temp = []
+# temp = []
 
-for num in range(len(res)-1):
-    temp.append(res[num+1]-res[num])
+# for num in range(len(res)-1):
+#     temp.append(res[num+1]-res[num])
 
-print(sum(temp))
+# print(sum(temp))
 
 # найти первое вхождение числа в строку, чтобы далее суммировать общее затраченной время
-result_time = []
-with open(BASE_DIR, 'r', encoding='utf-8') as file:
-    for line in file:
-        if 'Время расчета' in line:
-            hour, min, sec = line[-10:-2].split('.')
-            print(hour, min, sec)
+# result_time = []
+# with open(BASE_RESULT_DIR, 'r', encoding='utf-8') as file:
+#     for line in file:
+#         if 'Время расчета' in line:
+#             hour, min, sec = line[-10:-2].split('.')
+#             print(hour, min, sec)
             # temp = line.find()
             # result_time.append(time.strptime(line[-10:-2], '%H.%M.%S'))
 

86a5b6a2cd270de25653b92e32480c400288903e
CollapsingTime <vladislavrumiantsev@gmail.com>
2024-04-26 16:39:13 +0300

    Create function for reading files

--- read_results.py
+++ read_results.py
@@ -12,16 +12,44 @@ class Read_results:
         self.path = path
         self.data = data
 
-    def read_path(self, all_dirs: set = set()) -> set:
+    def read_path(self, all_dirs: set = set()) -> list:
         """
         Function pick up and collect all files name in directory
         """
         for root, dirs, files in os.walk(self.path):
             all_dirs.update(dirs)
-        return all_dirs
+        return list(all_dirs)
+    
+    def read_file(self, file_names: list = None) -> list:
+        """
+        Function can reading all result.log files in all dirs
+        """
+        file_names = self.read_path()
+        
+        for name in file_names:
+            with open(f"{BASE_RESULT_DIR}/{name}/result.log", 'r', encoding='utf-8') as file:
+                temp_data = []
+                for line in file:
+                    if 'TGP=' in line:
+                        start = line.find(' TGP=')
+                        end = line.find(', TGPH')
+                        temp_data.append(float(line[start+5:end]))
+                
+                temp_result = []
+                for num in range(len(temp_data)-1):
+                    temp_result.append(temp_data[num+1]-temp_data[num])
+
+                self.data.setdefault(name, {}).setdefault('GAS', temp_result)
+
 
 test = Read_results(BASE_RESULT_DIR)
-test.read_path()
+# print(test.read_path())
+test.read_file()
+
+print(test.data)
+
+for key, value in test.data.items():
+    print(f"{key} === {sum(value['GAS'])}", sep='\n')
 
 # count = 0
 # for root, dirs, files in os.walk(BASE_RESULT_DIR):

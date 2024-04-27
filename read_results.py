240f99bfc581eacac0a5ee8c35e561e931b421b0
CollapsingTime <vladislavrumiantsev@gmail.com>
2024-04-27 15:20:33 +0300

    Fix 'read_file' function

--- read_results.py
+++ read_results.py
@@ -12,13 +12,21 @@ class Read_results:
         self.path = path
         self.data = data
 
+    @staticmethod
+    def discount_volume(ls: list = None) -> list:
+        """
+        Create a discount volume from production
+        """
+        discount = [(1/pow(1+0.14, year)) for year in range(25)]
+        return discount
+
     def read_path(self, all_dirs: set = set()) -> list:
         """
         Function pick up and collect all files name in directory
         """
         for root, dirs, files in os.walk(self.path):
             all_dirs.update(dirs)
-        return list(all_dirs)
+        return sorted(list(all_dirs))
     
     def read_file(self, file_names: list = None) -> list:
         """
@@ -35,72 +43,19 @@ class Read_results:
                         end = line.find(', TGPH')
                         temp_data.append(float(line[start+5:end]))
                 
-                temp_result = []
-                for num in range(len(temp_data)-1):
-                    temp_result.append(temp_data[num+1]-temp_data[num])
+                for num in range(1, len(temp_data)):
+                    temp_data[-num] = (temp_data[-num]-temp_data[-num-1])
 
-                self.data.setdefault(name, {}).setdefault('GAS', temp_result)
+                temp_res = [None for _ in range(25)]
+                count = 0
+                for num in range(1, len(temp_data)-1, 12):
+                    temp_res[count] = sum(temp_data[num:num+12])
+                    count += 1
 
+                self.data.setdefault(name, {}).setdefault('GAS', temp_res)
 
 test = Read_results(BASE_RESULT_DIR)
-# print(test.read_path())
 test.read_file()
 
-print(test.data)
-
 for key, value in test.data.items():
-    print(f"{key} === {sum(value['GAS'])}", sep='\n')
-
-# count = 0
-# for root, dirs, files in os.walk(BASE_RESULT_DIR):
-#     # while count < 7:
-#         # You should change / or \ depending on OS
-#         with open(f"{BASE_RESULT_DIR}/{dirs[count]}/result.log", 'r', encoding='utf-8') as file:
-#             temp = []
-#             for line in file:
-#                 if 'TGP=' in line:
-#                     start = line.find(' TGP=')
-#                     end = line.find(', TGPH')
-#                     temp_res = line[start+5:end]
-#                     temp.append(float(temp_res))
-#             temp_res = []
-#             for num in range(len(temp)-1):
-#                 temp_res.append(temp[num+1]-temp[num])
-
-#             TOTAL_RESULTS.setdefault(dirs[count], {}).setdefault('GAS', temp_res)
-#         count += 1
-
-# print(TOTAL_RESULTS)
-# print(sum(TOTAL_RESULTS['CASE_254__PERM_10__Len_500__H_5__C5_300']['GAS']))
-
-# res = []
-
-# with open(BASE_DIR, 'r', encoding='utf-8') as file:
-#     for line in file:
-#         if 'TGP=' in line:
-#             start = line.find(' TGP=')
-#             end = line.find(', TGPH')
-#             temp = line[start+5:end]
-#             res.append(float(temp))
-
-# print(res)
-
-# temp = []
-
-# for num in range(len(res)-1):
-#     temp.append(res[num+1]-res[num])
-
-# print(sum(temp))
-
-# найти первое вхождение числа в строку, чтобы далее суммировать общее затраченной время
-# result_time = []
-# with open(BASE_RESULT_DIR, 'r', encoding='utf-8') as file:
-#     for line in file:
-#         if 'Время расчета' in line:
-#             hour, min, sec = line[-10:-2].split('.')
-#             print(hour, min, sec)
-            # temp = line.find()
-            # result_time.append(time.strptime(line[-10:-2], '%H.%M.%S'))
-
-# print(type(result_time[0]))
-
+    print(f"{key} === {sum(value['GAS'])} === {len(value['GAS'])}", sep='\n')

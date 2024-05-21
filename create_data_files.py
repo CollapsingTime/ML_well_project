2e3b69aa7e0d89acf8783b712c6fa7a1d2157130
CollapsingTime <vladislavrumiantsev@gmail.com>
2024-05-21 14:57:44 +0300

    Add vertical well and function for generate data files

--- create_data_files.py
+++ create_data_files.py
@@ -15,7 +15,7 @@ STATIC_DATA = {
     "Z": 0.4,
     "PERM": [0.1, 1, 10, 100, 1000],
     "H": [5, 10, 15, 20, 25],
-    "L": [500, 1000, 1500, 2000, 2500],
+    "L": [0, 500, 1000, 1500, 2000, 2500],
     "C5_plus": [0, 100, 200, 300, 400],
 }
 class GenerateInfoForModels:
@@ -107,6 +107,17 @@ class GenerateInfoForModels:
             # this type of cycles is slower than classic nested cycles
             num = next(item)
             match len:
+                case 0:
+                    self.dyn_data.setdefault(num, {'I': self.st_data['I'][0],
+                                                   'J': self.st_data['J'][0],
+                                                   'Hectare': self.st_data['Hectare'][0],
+                                                   'Well_path': self.st_data["SCH_path"][f"MODEL_SCHEDULE_{len}.inc"],
+                                                   'C5+_path': self.st_data["PVT_path"][f"MODEL_PROPS_{c5}.inc"],
+                                                   'PERM': perm,
+                                                   'H': thick,
+                                                   'L': len,
+                                                   'C5_plus': c5,
+                                                   'NTG': thick/32,})
                 case 500:
                     self.dyn_data.setdefault(num, {'I': self.st_data['I'][0],
                                                    'J': self.st_data['J'][0],
@@ -116,7 +127,8 @@ class GenerateInfoForModels:
                                                    'PERM': perm,
                                                    'H': thick,
                                                    'L': len,
-                                                   'C5_plus': c5})
+                                                   'C5_plus': c5,
+                                                   'NTG': thick/32,})
                 case 1000:
                     self.dyn_data.setdefault(num, {'I': self.st_data['I'][1],
                                                    'J': self.st_data['J'][1],
@@ -126,7 +138,8 @@ class GenerateInfoForModels:
                                                    'PERM': perm,
                                                    'H': thick,
                                                    'L': len,
-                                                   'C5_plus': c5})
+                                                   'C5_plus': c5,
+                                                   'NTG': thick/32,})
                 case 1500:
                     self.dyn_data.setdefault(num, {'I': self.st_data['I'][2],
                                                    'J': self.st_data['J'][2],
@@ -136,7 +149,8 @@ class GenerateInfoForModels:
                                                    'PERM': perm,
                                                    'H': thick,
                                                    'L': len,
-                                                   'C5_plus': c5})
+                                                   'C5_plus': c5,
+                                                   'NTG': thick/32,})
                 case 2000:
                     self.dyn_data.setdefault(num, {'I': self.st_data['I'][3],
                                                    'J': self.st_data['J'][3],
@@ -146,7 +160,8 @@ class GenerateInfoForModels:
                                                    'PERM': perm,
                                                    'H': thick,
                                                    'L': len,
-                                                   'C5_plus': c5})
+                                                   'C5_plus': c5,
+                                                   'NTG': thick/32,})
                 case 2500:
                     self.dyn_data.setdefault(num, {'I': self.st_data['I'][4],
                                                    'J': self.st_data['J'][4],
@@ -156,7 +171,8 @@ class GenerateInfoForModels:
                                                    'PERM': perm,
                                                    'H': thick,
                                                    'L': len,
-                                                   'C5_plus': c5})
+                                                   'C5_plus': c5,
+                                                   'NTG': thick/32,})
 
         with open(f'{BASE_RESULT_DIR}/data_from_realisations.txt', 'w', encoding='utf-8') as file:
             file.write(json.dumps(self.dyn_data, indent=4))
@@ -165,49 +181,27 @@ class GenerateInfoForModels:
         """
         Copy default data file and replace information
         """
-        pass
+        for case, params in self.dyn_data.items():
+            # print(f"case {case}, params {params}")
+            with open('/home/vladislav/data/ML_well_project/_DATA/CASES/test_temp.data', 'r', encoding='utf-8') as file_temp:
+                with open(f"/home/vladislav/data/ML_well_project/_TEMP_DATA/{case}_CASE__PERM_{params['PERM']}__Len_{params['L']}__H_{params['H']}__C5_{params['C5_plus']}.data", 'w', encoding='utf-8') as file:
+                    for line in file_temp:
+                        if '17 10 80 /' in line:
+                            file.write(line.replace('17 10 80 /', f"{params['I']} {params['J']} 80 /"))
+                        elif '1 17 1 10' in line:
+                            file.write(line.replace('1 17 1 10', f"1 {params['I']} 1 {params['J']}"))
+                        elif 'NTG=1' in line:
+                            file.write(line.replace('NTG=1', f"NTG={params['NTG']}"))
+                        elif 'PERMX=0.1' in line:
+                            file.write(line.replace('PERMX=0.1', f"PERMX={params['PERM']}"))
+                        elif 'INCLUDE/PVT/MODEL_PROPS_0.inc' in line:
+                            file.write(line.replace('INCLUDE/PVT/MODEL_PROPS_0.inc', f"{params['C5+_path']}"))
+                        elif 'INCLUDE/SCH/MODEL_SCHEDULE_500.inc' in line:
+                            file.write(line.replace('INCLUDE/SCH/MODEL_SCHEDULE_500.inc', f"{params['Well_path']}"))
+                        else:
+                            file.write(line)
 
 test = GenerateInfoForModels(STATIC_DATA)
 test.generate_info()
 test.generate_data_files_info()
-print(test.calculate_hectare())
-print(test.st_data['Hectare'])
-
-# 1. Add length 0 meters in L list
-# 2. Calculate Q/hectare/CAPEX column
-# 3. Copy and replace info function
-
-# # Replace all needed info in new file
-# with open("E:\ml_models\ALL_CASES\CASES\\test_temp.data", 'r', encoding='utf-8') as file_temp:
-#     with open("E:\ml_models\ALL_CASES\CASES\\test.data", 'w', encoding='utf-8') as file:
-#         for line in file_temp:
-#             if '17 10 80 /' in line:
-#                 file.write(line.replace('17 10 80 /', f'{I[4]} {J[4]} 80 /'))
-#             elif '1 17 1 10' in line:
-#                 file.write(line.replace('1 17 1 10', f'1 {I[4]} 1 {J[4]}'))
-#             elif 'NTG=1' in line:
-#                 file.write(line.replace('NTG=1', f'NTG={NTG[4]}'))
-#             elif 'PERMX=0.1' in line:
-#                 file.write(line.replace('PERMX=0.1', f'PERMX={K_PERM[4]}'))
-#             else:
-#                 file.write(line)
-
-# for case, params in ALL_REALISATIONS.items():
-#     temp_dir = case
-#     with open("E:\ml_models\ALL_CASES\\base_case_0.data", 'r', encoding='utf-8') as file_temp:
-#         with open(f"E:\ml_models\ALL_CASES\\CASE_{temp_dir}__PERM_{params['PERM']}__Len_{params['L']}__H_{params['H']}__C5_{params['C5+']}.data", 'w', encoding='utf-8') as file:
-#             for line in file_temp:
-#                 if '17 10 80 /' in line:
-#                     file.write(line.replace('17 10 80 /', f"{params['I']} {params['J']} 80 /"))
-#                 elif '1 17 1 10' in line:
-#                     file.write(line.replace('1 17 1 10', f"1 {params['I']} 1 {params['J']}"))
-#                 elif 'NTG=1' in line:
-#                     file.write(line.replace('NTG=1', f"NTG={params['NTG']}"))
-#                 elif 'PERMX=0.1' in line:
-#                     file.write(line.replace('PERMX=0.1', f"PERMX={params['PERM']}"))
-#                 elif 'INCLUDE/PVT/MODEL_PROPS_0.inc' in line:
-#                     file.write(line.replace('INCLUDE/PVT/MODEL_PROPS_0.inc', f"{params['C5+_path']}"))
-#                 elif 'INCLUDE/SCH/MODEL_SCHEDULE_500.inc' in line:
-#                     file.write(line.replace('INCLUDE/SCH/MODEL_SCHEDULE_500.inc', f"{params['Well_path']}"))
-#                 else:
-#                     file.write(line)
+test.create_data_files()

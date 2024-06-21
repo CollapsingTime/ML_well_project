f1fafbc28404fdaa0aec730294a0b5866e457978
Vladislav Rumiantsev <vladislavrumiantsev@gmail.com>
2024-06-21 15:15:16 +0300

    Create function create_only_result_dir

--- read_results.py
+++ read_results.py
@@ -1,15 +1,20 @@
 import os
 import json
+import shutil
 
 # Put in your RESULT directory
-BASE_RESULT_DIR = '/home/vladislav/Data/Backend/ML_well_project/_DATA/MODEL_DATA'
+BASE_RESULT_DIR = '/home/vladislav/Data/Backend/ML_well_project/_DATA/RESULTS_MODEL_DATA'
+
+# Here will be only result file for each path
+ONLY_RESULTS = '/home/vladislav/Data/Backend/ML_well_project/_ONLY_RESULTS_FILE'
 
 TOTAL_RESULTS = dict()
 
 class ReadResults:
-    def __init__(self, path: str, data: dict = {}):
-        if isinstance(path, str):
-            self.path = path
+    def __init__(self, base_path: str, result_path: str, data: dict = {}):
+        if isinstance(base_path, str) and isinstance(result_path, str):
+            self.base_path = base_path
+            self.result_path = result_path
         else:
             raise TypeError('Path must be a str type')
         self.data = data
@@ -31,7 +36,7 @@ class ReadResults:
         Calculate volume of gas with CAPEX and hectare
         """
         dump_datafile = {}
-        with open('data_from_realisations.txt', 'r') as file:
+        with open('/home/vladislav/Data/Backend/ML_well_project/data_from_realisations.txt', 'r') as file:
             dump_datafile = json.load(file)
         return num/dump_datafile[case]['Hectare']
 
@@ -46,18 +51,29 @@ class ReadResults:
         """
         Function picks up, collects and sorts all files name in directory
         """
-        for dir in next(os.walk(self.path))[1]:
+        for dir in next(os.walk(self.base_path))[1]:
             case = [num for num in dir.split('_') if num != '']
             all_dirs.setdefault(int(case[0]), dir)
 
         result_dirs = {k: v for k, v in sorted(all_dirs.items(), key=lambda item: item[0])}
         return result_dirs
     
+    def create_only_result_dir(self):
+        if not os.path.exists(self.result_path):
+            os.makedirs(self.result_path)
+
+        for k, v in self.read_result_path().items():
+            if not os.path.exists(f"{self.result_path}/{v}"):
+                os.makedirs(f"{self.result_path}/{v}")
+            file = f"{self.base_path}/{v}/result.log"
+            dir = f"{self.result_path}/{v}/result.log"
+            shutil.copy2(file, dir)
+
     def read_file(self, file_names: list = None) -> list:
         """
         Function can reading all result.log files in all dirs
         """
-        file_names = self.read_path()
+        file_names = self.read_result_path()
         
         for name in file_names:
             with open(f"{BASE_RESULT_DIR}/{name}/result.log", 'r', encoding='utf-8') as file:
@@ -97,10 +113,10 @@ class ReadResults:
         with open('result_file.txt', 'w', encoding='utf-8') as file:
             file.write(json.dumps(data, indent=4))
 
-test = ReadResults(BASE_RESULT_DIR)
+test = ReadResults(BASE_RESULT_DIR, ONLY_RESULTS)
 
 # print(test.read_result_path())
 for k, v in test.read_result_path().items():
     print(f"{k} --- {v}")
 
-# test.create_result_file()
+test.create_only_result_dir()
